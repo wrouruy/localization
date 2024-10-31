feee762070fe49076abde2fdf962e9c32d735ea1
wrouruy <161200383+wrouruy@users.noreply.github.com>
2024-10-31 10:02:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,161 @@
+let semanticCore = {
+    home: {
+        "ua": "Домашня",
+        "en": "Home",
+        "pl": "Strona główna",
+        "jp": "ホーム",
+        "fr": "Accueil",
+        "lt": "Pagrindinis",
+        "pt": "Início",
+        "es": "Inicio",
+        "hu": "Főoldal",
+        "zh": "主页",
+        "ar": "الصفحة الرئيسية",
+        "nl": "Thuis"
+    },
+    about: {
+        "ua": "Про нас",
+        "en": "About",
+        "pl": "O nas",
+        "jp": "私たちについて",
+        "fr": "À propos",
+        "lt": "Apie mus",
+        "pt": "Sobre",
+        "es": "Acerca de",
+        "hu": "Rólunk",
+        "zh": "关于我们",
+        "ar": "معلومات عنا",
+        "nl": "Over ons"
+    },
+    contact: {
+        "ua": "Контакти",
+        "en": "Contact",
+        "pl": "Kontakt",
+        "jp": "お問い合わせ",
+        "fr": "Contact",
+        "lt": "Kontaktai",
+        "pt": "Contacto",
+        "es": "Contacto",
+        "hu": "Kapcsolat",
+        "zh": "联系我们",
+        "ar": "اتصل بنا",
+        "nl": "Contact"
+    },
+    titleText: {
+        "ua": "Шукаєте когось, хто б правильно, коротко та якісно переклав ваш матеріал? Вам пощастило опинитися тут! Ми можемо перекласти все на понад 20 мов! Наші люди дуже освічені та розумні. Наша команда складається з носіїв мови та лінгвістів-поліглотів, які забезпечать вам надійний та зрозумілий переклад. Наша сучасна команда забезпечить вам розвиток і просування будь-якого бізнесу або матеріалу.",
+        "en": "Looking for someone to translate your material accurately, concisely, and with high quality? You’re in the right place! We can translate everything into more than 20 languages! Our people are highly educated and intelligent. Our team consists of native speakers and polyglot linguists who will ensure reliable and clear translations. Our modern team will support the growth and promotion of any business or material.",
+        "pl": "Szukasz kogoś, kto przetłumaczy Twój materiał poprawnie, zwięźle i jakościowo? Masz szczęście, że jesteś tutaj! Możemy przetłumaczyć wszystko na ponad 20 języków! Nasi ludzie są bardzo wykształceni i inteligentni. Nasz zespół składa się z rodzimych użytkowników języka i poliglotów-lingwistów, którzy zapewnią niezawodne i zrozumiałe tłumaczenie. Nasz nowoczesny zespół zapewni rozwój i promocję każdej działalności lub materiału.",
+        "jp": "あなたの素材を正確に、簡潔に、高品質に翻訳してくれる人をお探しですか？ここに来て正解です！私たちは20以上の言語に翻訳することができます！私たちのチームは非常に教育を受けたインテリ集団で、ネイティブスピーカーやポリグロットの言語学者で構成されています。信頼性の高い、わかりやすい翻訳を提供します。どんなビジネスや素材の発展とプロモーションもお手伝いします。",
+        "fr": "Vous cherchez quelqu'un pour traduire votre matériel avec précision, concision et qualité? Vous êtes au bon endroit! Nous pouvons tout traduire en plus de 20 langues! Nos traducteurs sont très instruits et intelligents. Notre équipe est composée de locuteurs natifs et de linguistes polyglottes qui vous garantiront une traduction fiable et claire. Notre équipe moderne vous aidera à développer et promouvoir n'importe quel business ou contenu.",
+        "lt": "Ieškote, kas galėtų tiksliai, trumpai ir kokybiškai išversti jūsų medžiagą? Jums pasisekė, kad atsidūrėte čia! Mes galime išversti į daugiau nei 20 kalbų! Mūsų žmonės yra labai išsilavinę ir protingi. Mūsų komanda susideda iš gimtosios kalbos vartotojų ir poliglotų lingvistų, kurie užtikrins patikimą ir aiškų vertimą. Mūsų moderni komanda užtikrins bet kokio verslo ar medžiagos augimą ir reklamą.",
+        "pt": "Procurando alguém para traduzir seu material com precisão, concisão e alta qualidade? Você está no lugar certo! Podemos traduzir tudo para mais de 20 idiomas! Nossa equipe é composta por pessoas altamente instruídas e inteligentes. Contamos com falantes nativos e linguistas poliglotas que garantirão traduções confiáveis e claras. Nossa equipe moderna apoiará o crescimento e a promoção de qualquer negócio ou material.",
+        "es": "¿Buscas a alguien que traduzca tu material de manera precisa, concisa y de alta calidad? ¡Estás en el lugar correcto! Podemos traducir todo a más de 20 idiomas. Nuestro equipo está compuesto por personas altamente capacitadas e inteligentes. Contamos con hablantes nativos y lingüistas políglotas que asegurarán traducciones confiables y claras. Nuestro equipo moderno apoyará el crecimiento y la promoción de cualquier negocio o material.",
+        "hu": "Olyan fordítót keres, aki pontosan, röviden és minőségien fordítja le az anyagát? Szerencséje van, hogy itt van! Több mint 20 nyelvre le tudunk fordítani mindent! Csapatunk kiválóan képzett és intelligens emberekből áll. Anyanyelvi beszélők és poliglott nyelvészek biztosítják a megbízható és érthető fordítást. Modern csapatunk elősegíti bármely üzlet vagy anyag fejlődését és népszerűsítését.",
+        "zh": "在寻找能够准确、简洁、高质量地翻译您的材料的人吗？您来对地方了！我们可以将所有内容翻译成超过20种语言！我们团队由高度教育且智能的成员组成。我们的团队包括母语者和精通多语言的语言学家，能够确保可靠且清晰的翻译。我们的现代团队将帮助您推动和推广任何业务或材料。",
+        "ar": "هل تبحث عن شخص يترجم موادك بدقة وباختصار وبجودة عالية؟ أنت في المكان المناسب! يمكننا ترجمة كل شيء إلى أكثر من 20 لغة! أعضاء فريقنا متعلمون للغاية وذو كفاءة. يتكون فريقنا من متحدثين أصليين ولغويين متعددي اللغات سيضمنون لك ترجمة موثوقة وواضحة. سيساعد فريقنا العصري في تطوير وترويج أي عمل أو مادة.",
+        "nl": "Op zoek naar iemand die uw materiaal nauwkeurig, bondig en van hoge kwaliteit kan vertalen? U bent hier aan het juiste adres! Wij kunnen alles vertalen in meer dan 20 talen! Ons team bestaat uit hoogopgeleide en slimme mensen. Onze teamleden zijn moedertaalsprekers en polyglotte taalkundigen die zorgen voor betrouwbare en duidelijke vertalingen. Ons moderne team ondersteunt de groei en promotie van elk bedrijf of materiaal."
+    },
+    hello: {
+        "ua": "Привіт!",
+        "en": "Hello!",
+        "pl": "Czesc!",
+        "jp": "こんにちは！",
+        "fr": "Salutations!",
+        "lt": "Sveikinimai!",
+        "pt": "Parabéns!",
+        "es": "¡Saludos!",
+        "hu": "Üdvözlet!",
+        "zh": "问候！",
+        "ar": "تحيات!",
+        "nl": "Groeten!"
+    }
+};
+
+
+
+let allLang = ['en', 'ua', 'pl', 'jp', 'fr', 'lt', 'pt', 'es', 'hu', 'zh', 'ar', 'nl'];
+let lang
+
+function changeURL() {
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+// changeURL();
+function changeLanguage() {
+    let hash = (window.location.hash).substring(1);
+    // console.log(hash);
+
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    for (let key in semanticCore) {
+        $('.language-' + key).text(semanticCore[key][hash]);
+        // console.log(key);
+    }
+}
+changeLanguage();
+
+
+$('.lang').click(function(e) {
+    const targetDiv = $(e.target).closest('div[id]');
+    if (targetDiv.length) {
+        lang = targetDiv.attr('id');
+        changeURL();
+    }
+});
+
+
+
+
+$(document).ready(function() {
+    if (localStorage.getItem('themechange_localization') === 'black') {
+
+        $('.header').css('borderBottom', '2px solid rgb(132, 132, 132)')
+        $('.wrap').css('backgroundColor', 'rgb(26, 26, 26)')
+        $('.language-titleText, .language-hello').css('color', 'white')
+        $('.language-titleText').css('borderColor', 'white')
+      
+        $('.changeThemeBTN div').css('left', '23px')
+    } else if(localStorage.getItem('themechange_localization') === 'white'){
+     
+        $('.header').css('borderBottom', 'transparent 2px solid')
+        $('.wrap').css('backgroundColor', 'transparent')
+        $('.language-titleText, .language-hello').css('color', 'black')
+        $('.language-titleText').css('borderColor', 'black')
+      
+        $('.changeThemeBTN div').css('left', '-23px')
+    }
+    setTimeout(function(){
+        $('.changeThemeBTN div').css('transition', '300ms');
+    }, 300)
+
+    $('.changeThemeBTN').click(function() {
+        const currentTheme = localStorage.getItem('themechange_localization');
+        const newTheme = currentTheme === 'white' ? 'black' : 'white';
+    
+        localStorage.setItem('themechange_localization', newTheme);
+    
+        const newLeft = newTheme === 'black' ? '23px' : '-23px';
+        $('.changeThemeBTN div').css({ left: newLeft, width: '65px' });
+    
+        if (newTheme === 'black') {
+            $('.header').css('borderBottom', '2px solid rgb(132, 132, 132)');
+            $('.wrap').css('backgroundColor', 'rgb(26, 26, 26)');
+            $('.language-titleText, .language-hello').css('color', 'white');
+            $('.language-titleText').css('borderColor', 'white');
+        } else {
+            $('.header').css('borderBottom', 'transparent 2px solid');
+            $('.wrap').css('backgroundColor', 'transparent');
+            $('.language-titleText, .language-hello').css('color', 'black');
+            $('.language-titleText').css('borderColor', 'black');
+        }
+    
+        setTimeout(function() {
+            $('.changeThemeBTN div').css('width', '26px');
+        }, 100);
+    });
+    
+});
+
+
